2a977ed58183812b47ac39e84d35cc58fb5a6dcc
MarkRywell <gopro855@gmail.com>
2024-07-02 17:02:43 +0800

    ci: :wrench: Update config of pm2

--- pm2.config.js
+++ pm2.config.js
@@ -7,11 +7,4 @@ module.exports = {
     //combine multiple err/out logs in one file for each
     combine_logs: true,
     //calls combine logs
-    merge_logs: true,
-    //error log file path
-    error_file: "logs/err.log", // better be /var/log
-    //out log file path
-    out_file: "logs/out.log",
-    // use time in logs
-    time: true,
 }
