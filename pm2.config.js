182cd630d1c2e16ff14874c416b95f299a9d8f78
MarkRywell <gopro855@gmail.com>
2024-07-02 17:19:30 +0800

    ci: :wrench: Remove pm2 logs option

--- pm2.config.js
+++ pm2.config.js
@@ -4,7 +4,4 @@ module.exports = {
     ignore_watch: ["node_modules"],
     // new feature; increase restart delay each time after every crash or non reachable db per example
     exp_backoff_restart_delay: 100,
-    //combine multiple err/out logs in one file for each
-    combine_logs: true,
-    //calls combine logs
 }
